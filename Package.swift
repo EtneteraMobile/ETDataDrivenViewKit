4cff2814a7be57de550f00eebc479b6b996ed19c
Bohdan Kubkin <bohdan.kubkin@etnetera.cz>
2019-12-10 17:08:59 +0100

    🐛 Repaired invalid SPM package definition

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ETDataDrivenViewKit",
+    platforms: [.iOS(.v9), .macOS(.v10_10), .watchOS(.v2), .tvOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,13 +15,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ETDataDrivenViewKit",
-            path: "Sources"),
+            dependencies: ["Differentiator"],
+            path: "Sources"
+        ),
         .testTarget(
             name: "ETDataDrivenViewKitTests",
             dependencies: ["ETDataDrivenViewKit"]),
